dd02bcbef72e2e37786f3e2dc1316bad1633d26b
rahul chavan <rcsofttech85@gmail.com>
2018-06-09 20:22:24 +0530

    run test

--- symfony/bin/deploy/pre_deploy.sh
+++ symfony/bin/deploy/pre_deploy.sh
@@ -1,7 +1,20 @@
-# Remove symlink
-sudo rm -R /var/www/micropost_old && \
-sudo cp -R /var/www/micropost_current /var/www/micropost_old/ && \
-sudo rm /var/www/micropost && \
-sudo rm -R /var/www/micropost_current && \
-# Create symlink to older version && \
-sudo ln -s /var/www/micropost_old /var/www/micropost
+# env variable
+SYMFONY_APP_PATH=$SYMFONY_APP_PATH
+
+# PHP/nginx
+TIMEZONE=$TIMEZONE
+MAX_EXECUTION_TIME=$MAX_EXECUTION_TIME
+
+# MySQL
+MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+MYSQL_DATABASE=$MYSQL_DATABASE
+MYSQL_USER=$MYSQL_USER
+MYSQL_PASSWORD=$MYSQL_PASSWORD
+
+# RabbitMQ
+RABBITMQ_USER=rabbit
+RABBITMQ_PASSWORD=mq
+
+# prod
+DATABASE_URL=$DATABASE_URL
+APP_ENV=$APP_ENV
