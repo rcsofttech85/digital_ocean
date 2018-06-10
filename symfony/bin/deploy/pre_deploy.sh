899577afdce271417f8f548951b96b0d7260a8a7
rahul chavan <rcsofttech85@gmail.com>
2018-06-10 14:08:06 +0530

    final

--- symfony/bin/deploy/pre_deploy.sh
+++ symfony/bin/deploy/pre_deploy.sh
@@ -9,27 +9,3 @@ docker exec container_phpfpm yarn config set ignore-engines true
 docker exec container_phpfpm yarn install
 docker exec container_phpfpm yarn run encore production
 docker exec -e DATABASE_URL=$DATABASE_URL  container_phpfpm bin/console doctrine:migrations:migrate --no-interaction
-
-
-
-
-# env variable
-SYMFONY_APP_PATH=$SYMFONY_APP_PATH
-
-# PHP/nginx
-TIMEZONE=$TIMEZONE
-MAX_EXECUTION_TIME=$MAX_EXECUTION_TIME
-
-# MySQL
-MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
-MYSQL_DATABASE=$MYSQL_DATABASE
-MYSQL_USER=$MYSQL_USER
-MYSQL_PASSWORD=$MYSQL_PASSWORD
-
-# RabbitMQ
-RABBITMQ_USER=rabbit
-RABBITMQ_PASSWORD=mq
-
-# prod
-DATABASE_URL=$DATABASE_URL
-APP_ENV=$APP_ENV
