1c481c1b7008cdc68a29632cf2c18073eb872f76
rahul chavan <rcsofttech85@gmail.com>
2018-06-10 15:50:21 +0530

    final

--- symfony/bin/deploy/pre_deploy.sh
+++ symfony/bin/deploy/pre_deploy.sh
@@ -1,12 +1,1 @@
-cd digital_ocean
-git pull origin master
-docker-compose down
-docker-compose up -d --build
-docker exec container_phpfpm composer install -n --prefer-dist
-docker exec container_phpfpm bin/console cache:clear --env=prod --no-interaction
-docker exec container_phpfpm bin/console cache:warmup --env=prod --no-interaction
-docker exec container_phpfpm yarn config set ignore-engines true
-docker exec container_phpfpm yarn install
-docker exec container_phpfpm yarn run encore production
-docker exec -e APP_ENV=prod DATABASE_URL=mysql://symfony:symfony@db:3306 container_phpfpm bin/console doctrine:database:create
-docker exec -e APP_ENV=prod DATABASE_URL=mysql://symfony:symfony@db:3306 container_phpfpm bin/console doctrine:migrations:migrate --no-interaction
+
